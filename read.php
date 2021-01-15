b4cd171950de4fbf4a0411fd42670d87b6013151
AlexGGGG <73903996+AlexGGGG@users.noreply.github.com>
2021-01-15 19:23:26 +0200

    Add files via upload
    занятие по php от 15.01.2021

--- read.php
+++ read.php
@@ -1,6 +1,15 @@
 <?php
 include ('config.php');
-$checkSQL=mysqli_query($connection, "SELECT id, title, news FROM `news`");//`=Ё
+if(empty($_GET['id'])){
+	die ('Цель не была выбрана!');
+} else {
+	$id = $_GET['id'];
+//запрос
+	$checkSQL=mysqli_query($connection, "SELECT id, title, news FROM `news` WHERE id='$id'");
+	$sql = "SELECT * FROM news WHERE id='$id'";
+	$output  = mysqli_query($connection, $sql);
+	$str = mysqli_fetch_assoc($output);	
+}		
 ?>
 <!doctype html>
 <html>
@@ -9,17 +18,17 @@ $checkSQL=mysqli_query($connection, "SELECT id, title, news FROM `news`");//`=Ё
 <title>Чтение новостей</title>
 </head>
 <body>
+<h1>Просмотр новостей</h1>
 <?php
-    while($str= mysqli_fetch_array($checkSQL))
-    {
-        
-        echo '<div>
-                <p><span>ID </span>'.$str['id'].'</p>
-                <p><span>Title </span>'.$str['title'].'</p>
-                <p><span>News </span>'.$str['news'].'</p>
-            </div>
-            <hr>';
-    }
+	while($str= mysqli_fetch_array($checkSQL))
+	{	
+	echo'<div>
+		<p>'.$str['title'].'</p>
+		<p>'.$str['news'].'</p>
+		</div>';
+	}
 ?>
+<a href="news.php"> назад </a>
+
 </body>
 </html>
